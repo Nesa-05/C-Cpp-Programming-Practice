618d05a1f9d763d035648b11d07eaf0d030e7d5b
Nesa <1112nesa@gmail.com>
2026-09-19 09:53:32 +0600

    Ages in days

---
+++ CodeForces-Problems/03_Ages_in_days.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int n,years,months,days;
+    cin>>n;
+    years=n/365;
+    n=n%365;
+    months=n/30;
+    days=n%30;
+    cout<<years<<" "<<"years"<<endl;
+    cout<<months<<" "<<"months"<<endl;
+    cout<<days<<" "<<"days";
+    return 0;
+}
